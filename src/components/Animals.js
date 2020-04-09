bce82e5d89ebf57f6cf0236f840cc5a205454eef
NikolayK <nikolay.kim@codeimmersive.com>
2020-04-09 11:44:57 -0400

    adds 12 images and creates components

---
+++ src/components/Animals.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+
+
+const animals = [
+   {
+      image: 'fox1.jpg',
+      type: 'Vulpes',
+      name: 'Scout',
+      description: 'Scout is chilling on a snow bed',
+      animalId: 'fox1'
+   },
+   {
+      image: 'fox2.jpg',
+      type: 'Vulpes',
+      name: 'Autumn',
+      description: 'Autumn is ready to snack on your chicken',
+      animalId: 'fox2'
+   },
+   {
+      image: 'fox3.jpg',
+      type: 'Vulpes',
+      name: 'Kit',
+      description: 'Kit is not sure if she turned off the stove',
+      animalId: 'fox3'
+   },
+   {
+      image: 'fox4.jpg',
+      type: 'Vulpes',
+      name: 'Arvi',
+      description: 'Arvi is small, but determined',
+      animalId: 'fox4'
+   },
+   {
+      image: 'penguin1.jpg',
+      type: 'Aptenodytes',
+      name: 'Flipperstein',
+      description: 'Flipperstein tired of his fame',
+      animalId: 'penguin1'
+   },
+   {
+      image: 'penguin2.jpeg',
+      type: 'Aptenodytes',
+      name: 'Flapperson',
+      description: 'Flapperson is done with the quarantine',
+      animalId: 'penguin2'
+   },
+   {
+      image: 'penguin3.jpg',
+      type: 'Aptenodytes',
+      name: 'Fishface group',
+      description: 'Fishface group is known for their fishing abilities',
+      animalId: 'penguin3'
+   },
+   {
+      image: 'penguin4.jpg',
+      type: 'Aptenodytes',
+      name: 'Beakerson',
+      description: 'Beakerson is not sure about your fashion sense',
+      animalId: 'penguin4'
+   },
+   {
+      image: 'wolf1.jpg',
+      type: 'Canis',
+      name: 'Frost',
+      description: 'Frost is single and ready to mingle',
+      animalId: 'wolf1'
+   },
+   {
+      image: 'wolf2.jpg',
+      type: 'Canis',
+      name: 'Stalker',
+      description: 'Stalker is looking for his keys',
+      animalId: 'wolf2'
+   },
+   {
+      image: 'wolf3.jpg',
+      type: 'Canis',
+      name: 'Fang',
+      description: '"Maybe you shouldn\'t smile, Fang"',
+      animalId: 'wolf3'
+   },
+   {
+      image: 'wolf4.jpg',
+      type: 'Canis',
+      name: 'Sierra',
+      description: 'Sierra is looking for experienced nanny',
+      animalId: 'wolf4'
+   },
+
+]
+
+export default class Animals extends Component {
+   render() {
+      return (
+         <div>
+         </div>
+      )
+   }
+}
